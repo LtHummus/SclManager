35d843a339b0bb14c9c0f61afbd1ba9a17380f2f
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2016-07-21 21:23:14 -0700

    implemented some draft bullshit

--- src/main/scala/com/lthummus/sclmanager/database/dao/DraftDao.scala
+++ src/main/scala/com/lthummus/sclmanager/database/dao/DraftDao.scala
@@ -2,6 +2,7 @@ package com.lthummus.sclmanager.database.dao
 
 import org.jooq.DSLContext
 import zzz.generated.Tables
+import zzz.generated.tables.records.DraftRecord
 
 import scala.collection.JavaConversions._
 
@@ -21,4 +22,35 @@ object DraftDao {
     }
   }
 
+  def getLatestForPlayers(players: Seq[String])(implicit dslContext: DSLContext) = {
+    players.ensuring(players.length == 2)
+
+    val sortedPlayers = players.sorted
+
+    val res = dslContext.selectFrom(Tables.DRAFT)
+      .where(Tables.DRAFT.PLAYER1.eq(sortedPlayers.head))
+      .and(Tables.DRAFT.PLAYER2.eq(sortedPlayers(1)))
+      .orderBy(Tables.DRAFT.TIME.desc()).fetch()
+
+    res.size() match {
+      case 0 => None
+      case 1 => Some(res.get(0))
+      case _ => ???
+    }
+  }
+
+  def isDraftUsed(id: Int)(implicit dslContext: DSLContext) =
+    dslContext.selectFrom(Tables.BOUT).where(Tables.BOUT.DRAFT.eq(id)).fetch().size() != 0
+
+
+  private def killUsedDraft(draft: DraftRecord) = {
+    if (isDraftUsed(draft.getId))
+      None
+    else
+      Some(draft)
+  }
+
+  def getLatestUnusedDraftForPlayer(players: Seq[String])(implicit dslContext: DSLContext): Option[DraftRecord] = {
+    getLatestForPlayers(players).flatMap(killUsedDraft)
+  }
 }
