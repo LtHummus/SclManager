e57728ba8ee788abad8d07a7e159209503343977
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2016-10-03 22:04:47 -0700

    add authentication to draft post

--- src/main/scala/com/lthummus/sclmanager/servlets/DraftServlet.scala
+++ src/main/scala/com/lthummus/sclmanager/servlets/DraftServlet.scala
@@ -3,9 +3,10 @@ package com.lthummus.sclmanager.servlets
 import com.lthummus.sclmanager.SclManagerStack
 import com.lthummus.sclmanager.database.dao.DraftDao
 import com.lthummus.sclmanager.servlets.dto.{Draft, DraftInput}
+import com.typesafe.config.ConfigFactory
 import org.jooq.DSLContext
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.Ok
+import org.scalatra.{Forbidden, Ok}
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.swagger.{Swagger, SwaggerSupport}
 
@@ -19,17 +20,24 @@ class DraftServlet(implicit dslContext: DSLContext, val swagger: Swagger) extend
 
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
 
+  lazy val sharedSecret = ConfigFactory.load().getString("sharedSecret")
+
   before() {
     contentType = formats("json")
   }
 
 
   post("/report") {
-    val input = parsedBody.camelizeKeys.extract[DraftInput]
+    val auth = request.header("Authentication")
+    auth match {
+      case Some(x) if x == sharedSecret =>
+        val input = parsedBody.camelizeKeys.extract[DraftInput]
+        DraftDao.persist(input)
+        Ok("ok")
 
-    DraftDao.persist(input)
+      case _ => Forbidden(Map("error" -> "Nope"))
+    }
 
-    Ok("ok")
   }
 
 
