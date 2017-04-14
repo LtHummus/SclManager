fc17a4484b3bebf6a265271b01f6d3ccbb54c72a
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2017-04-13 21:05:01 -0700

    flags!

--- src/main/scala/com/lthummus/sclmanager/util/LeagueGenerator.scala
+++ src/main/scala/com/lthummus/sclmanager/util/LeagueGenerator.scala
@@ -7,42 +7,97 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
-object LeagueGenerator extends App {
+private object LeagueGenerator extends App {
   val Logger = LoggerFactory.getLogger(getClass)
 
-  type PlayerName = String
-  case class League(order: Int, name: String, players: Seq[PlayerName])
-  case class Match(player1: PlayerName, player2: PlayerName)
+  private case class Player(name: String, country: String)
+  private case class League(order: Int, name: String, players: Seq[Player])
+  private case class Match(player1: Player, player2: Player)
 
-  val Leagues = Seq(
-    League(0, "Diamond", Seq("krazycaley", "magician1099", "canadianbacon", "kcmmmmm", "bloom", "cleetose")),
-    League(1, "Platinum", Seq("slappydavis", "varanas", "sharper", "warningtrack", "drawnonward", "cameraman")),
-    League(2, "Gold", Seq("royalflush", "pires", "scallions", "james1221", "fearfulferret", "elvisnake")),
-    League(3, "Silver", Seq("sgnurf", "wodar", "teetery", "checker", "falconhit", "pwndnoob")),
-    League(4, "Bronze", Seq("yerand", "yeesh", "catnip", "cbot", "jyaty", "tarpshack")),
-    League(5, "Copper", Seq("bittersweet93", "nanthelas", "progamingwithed", "trevor", "tge", "gmantsang")),
-    League(6, "Iron", Seq("steph", "marimo", "baldrick", "ml726", "arisumargatroid", "poppe")),
-    League(7, "Challenger", Seq("danishspy", "dburke", "fourliberties", "anorexicwhale", "quicklime", "moon", "sailormoon92"))
+  private val Leagues = Seq(
+    League(0, "Diamond", Seq(Player("krazycaley", "us"),
+      Player("magician1099", "us"),
+      Player("canadianbacon", "ca"),
+      Player("kcmmmmm", "us"),
+      Player("bloom", "au"),
+      Player("cleetose", "us"))),
+
+    League(1, "Platinum",
+      Seq(Player("slappydavis", "us"),
+        Player("varanas", "gb"),
+        Player("sharper", "au"),
+        Player("warningtrack", "us"),
+        Player("drawnonward", "us"),
+        Player("cameraman", "us"))),
+
+    League(2, "Gold",
+      Seq(Player("royalflush", "gb"),
+        Player("pires", "pt"),
+        Player("scallions", "us"),
+        Player("james1221", "us"),
+        Player("fearfulferret", "us"),
+        Player("elvisnake", "us"))),
+
+    League(3, "Silver",
+      Seq(Player("sgnurf", "fr"),
+        Player("wodar", "us"),
+        Player("teetery", "us"),
+        Player("checker", "us"),
+        Player("falconhit", "ca"),
+        Player("pwndnoob", "gb"))),
+
+    League(4, "Bronze",
+     Seq(Player("yerand", "lv"),
+         Player("yeesh", "us"),
+         Player("catnip", "lt"),
+         Player("cbot", "au"),
+         Player("jyaty", "sg"),
+         Player("tarpshack", "us"))),
+
+    League(5, "Copper",
+      Seq(Player("bittersweet93", "gb"),
+        Player("nanthelas", "us"),
+        Player("progamingwithed", "us"),
+        Player("trevor", "us"),
+        Player("tge", "us"),
+        Player("gmantsang", "ca"))),
+
+    League(6, "Iron",
+      Seq(Player("steph", "ca"),
+        Player("marimo", "pl"),
+        Player("baldrick", "pl"),
+        Player("ml726", "us"),
+        Player("arisumargatroid", "pt"),
+        Player("poppe", "se"))),
+
+    League(7, "Challenger",
+      Seq(Player("danishspy", "dk"),
+        Player("dburke", "us"),
+        Player("fourliberties", "it"),
+        Player("anorexicwhale", "us"),
+        Player("quicklime", "us"),
+        Player("moon", "us"),
+        Player("sailormoon92", "us")))
   )
   val NumberOfTimesEveryonePlaysEachOther = 2
 
   implicit val Db: DSLContext = DatabaseConfigurator.getDslContext
 
-  def generatePairs(players: Seq[PlayerName]): List[(PlayerName, PlayerName)] = {
+  private def generatePairs(players: Seq[Player]): List[(Player, Player)] = {
     players.ensuring(_.length % 2 == 0)
     val reversedPlayers = players.reverse
     (for (x <- 0 until players.length / 2)
       yield (players(x), reversedPlayers(x))).toList
   }
 
-  private def rotate(xs: Seq[PlayerName], amount: Int) = xs.drop(amount) ++ xs.take(amount)
+  private def rotate(xs: Seq[Player], amount: Int) = xs.drop(amount) ++ xs.take(amount)
 
   private val ByeText = "BYE"
 
-  def generateWeeklySchedule(players: Seq[PlayerName]) = {
+  private def generateWeeklySchedule(players: Seq[Player]) = {
     val playersWithByeIfNeeded = players.size % 2 match {
       case 0 => players
-      case 1 => players ++ Seq(ByeText)
+      case 1 => players ++ Seq(Player(ByeText, "XX"))
     }
 
     val shuffledPlayers = scala.util.Random.shuffle(playersWithByeIfNeeded)
@@ -57,38 +112,38 @@ object LeagueGenerator extends App {
 
   }
 
-  private def containsBye(x: (String, String)): Boolean = x._1 == ByeText || x._2 == ByeText
+  private def containsBye(x: (Player, Player)): Boolean = x._1.name == ByeText || x._2.name == ByeText
 
-  def generateMatches(leagues: Seq[League]) = {
+  private def generateMatches(leagues: Seq[League]) = {
     leagues.map(it => (it, generateWeeklySchedule(it.players) ++ generateWeeklySchedule(it.players)))
   }
 
-  def populateWeekMatches(league: League, week: Int, matches: Iterable[(PlayerName, PlayerName)]) = {
+  private def populateWeekMatches(league: League, week: Int, matches: Iterable[(Player, Player)]) = {
     Logger.info(s"populating matches for ${league.name} week $week: ")
     for (curr <- matches) {
       Logger.info(s"${curr._1} vs. ${curr._2}")
     }
   }
 
-  def persistLeagueData(league: League, matches: Seq[List[(String, String)]]) = {
+  private def persistLeagueData(league: League, matches: Seq[List[(Player, Player)]]) = {
     //step 1: create the league
 
     val leagueRecord = new DivisionRecord(league.name, league.order)
     Db.executeInsert(leagueRecord)
 
     //step 2: insert the players
-    val playerRecords = league.players.map(new PlayerRecord(_, league.name, 0, 0, 0, "sp"))
+    val playerRecords = league.players.map(p => new PlayerRecord(p.name, league.name, 0, 0, 0, p.country))
     Db.batchInsert(playerRecords.asJava).execute()
 
     //step 3: insert all the matches
     for (week <- matches.indices) {
       val thisWeek = matches(week)
-      val matchRecords = thisWeek.map(it => new BoutRecord(null, week + 1, league.name, it._1, it._2, 0, null, null, null))
+      val matchRecords = thisWeek.map(it => new BoutRecord(null, week + 1, league.name, it._1.name, it._2.name, 0, null, null, null))
       Db.batchInsert(matchRecords.asJava).execute()
     }
   }
 
-  val allLeaguesXMatches = generateMatches(Leagues)
+  private val allLeaguesXMatches = generateMatches(Leagues)
 
   for {(league, matches) <- allLeaguesXMatches} {
     persistLeagueData(league, matches)
