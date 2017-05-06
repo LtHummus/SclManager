4f24b4926d471f9b7ef72b612a1d0d9b598465b4
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2017-05-05 20:59:50 -0700

    encrypt credentials in config file

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -3,17 +3,24 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
+import com.lthummus.sclmanager.scaffolding.SystemConfig._
+import org.slf4j.LoggerFactory
 
 object JettyLauncher {
 
+  private val Logger = LoggerFactory.getLogger("JettyLauncher")
+
   def main(args: Array[String]) {
+    Logger.info("Hello world!")
     val config = ConfigFactory.load()
-    val port = config.getInt("server.port")
+    val port = config.getIntWithStage("server.port")
+
+    Logger.info("Password: " + config.getEncryptedString("database.password"))
 
     val server = new Server(port)
     val context = new WebAppContext()
     context.setContextPath("/")
-    context.setResourceBase(config.getString("server.webroot"))
+    context.setResourceBase(config.getStringWithStage("server.webroot"))
     context.addEventListener(new ScalatraListener)
     context.addServlet(classOf[DefaultServlet], "/")
 
