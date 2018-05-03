471394b488cd425fd5f3fae7a22ffbbd5f059138
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2018-05-02 21:42:40 -0700

    added zip file level patching

--- src/main/scala/com/lthummus/sclmanager/parsing/PatchTester.scala
+++ src/main/scala/com/lthummus/sclmanager/parsing/PatchTester.scala
@@ -1,21 +1,30 @@
 package com.lthummus.sclmanager.parsing
 
-import java.io.{ByteArrayInputStream, DataInputStream}
+import java.io.FileOutputStream
 import java.nio.file.{Files, Paths}
 
+import org.apache.commons.io.IOUtils
 import scalaz.{-\/, \/-}
 
 object PatchTester extends App {
 
-  val file = "C:\\Users\\Benjamin\\AppData\\Local\\SpyParty\\replays\\Matches\\2018-04\\lthummus vs s76561198016757660%2fsteam - 20180429-13-13-34\\SpyPartyReplay-20180429-13-14-04-lthummus-vs-s76561198016757660%2fsteam-iP61gRp6T7CD9cbVcaJ8PA-v23.replay"
+  val oldZip = "C:\\Users\\Benjamin\\AppData\\Local\\SpyParty\\replays\\Matches\\2018-04\\lthummus vs s76561198016757660%2fsteam - 20180429-13-13-34\\test.zip"
 
-  val bytes = Files.readAllBytes(Paths.get(file))
+  val oldZipData = Files.readAllBytes(Paths.get(oldZip))
 
-  ReplayNamePatcher.patchReplay(new DataInputStream(new ByteArrayInputStream(bytes)), Map("lthummus" -> "captfalafel", "s76561198016757660/steam" -> "drmcsteamy")) match {
-    case -\/(error) => println(s"Something screwed up $error")
-    case \/-(newReplay) =>
-      Files.write(Paths.get("C:\\Users\\Benjamin\\AppData\\Local\\SpyParty\\replays\\Matches\\2018-04\\lthummus vs s76561198016757660%2fsteam - 20180429-13-13-34\\maybe.replay"), newReplay)
-      println("maybe?")
+  val nameChangeMap = Map(
+    "lthummus" -> "abcdefg",
+    "s76561198016757660/steam" -> "DPWSEY"
+  )
+
+  ZipFilePatcher.patchZipFile(oldZipData, nameChangeMap) match {
+    case -\/(error) => s"Error: $error"
+    case \/-(newData) =>
+      val os = new FileOutputStream("C:\\Users\\Benjamin\\AppData\\Local\\SpyParty\\replays\\Matches\\2018-04\\lthummus vs s76561198016757660%2fsteam - 20180429-13-13-34\\test2.zip")
+      IOUtils.copy(newData, os)
+      newData.close()
+      os.close()
+      println("Maybe!?")
   }
 
 }
---
+++ src/main/scala/com/lthummus/sclmanager/parsing/ZipFilePatcher.scala
@@ -0,0 +1,42 @@
+package com.lthummus.sclmanager.parsing
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, DataInputStream, InputStream}
+import java.util.zip.{ZipEntry, ZipInputStream, ZipOutputStream}
+
+import scalaz._
+import Scalaz._
+import org.apache.commons.io.IOUtils
+
+object ZipFilePatcher {
+
+  def patchZipFile(zipFileBytes: Array[Byte], nameChanges: Map[String, String]): String \/ InputStream = {
+    val baos = new ByteArrayOutputStream()
+    val zis = new ZipInputStream(new ByteArrayInputStream(zipFileBytes))
+    val zos = new ZipOutputStream(baos)
+
+    var inputEntry: ZipEntry = zis.getNextEntry
+
+    while (inputEntry != null) {
+      if (!inputEntry.isDirectory) {
+        val fileData = new DataInputStream(zis)
+        val newReplay = ReplayNamePatcher.patchReplay(fileData, nameChanges)
+
+        newReplay match {
+          case -\/(error) => return s"Error patching replay: $error".left
+          case \/-(data) =>
+            val newEntry = new ZipEntry(inputEntry.getName)
+            zos.putNextEntry(newEntry)
+            zos.write(data)
+            zos.closeEntry()
+        }
+
+        inputEntry = zis.getNextEntry
+      }
+    }
+
+    zis.close()
+    zos.close()
+
+    new ByteArrayInputStream(baos.toByteArray).right
+  }
+}
