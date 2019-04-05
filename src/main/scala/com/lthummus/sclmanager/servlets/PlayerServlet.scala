661053693ba44cee4087cdafde3052e60326a53d
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2019-04-04 20:46:18 -0700

    add bulk forfeit functionality

--- src/main/scala/com/lthummus/sclmanager/servlets/PlayerServlet.scala
+++ src/main/scala/com/lthummus/sclmanager/servlets/PlayerServlet.scala
@@ -2,13 +2,20 @@ package com.lthummus.sclmanager.servlets
 
 import com.lthummus.sclmanager.SclManagerStack
 import com.lthummus.sclmanager.database.dao.{BoutDao, DivisionDao, GameDao, PlayerDao}
+import com.lthummus.sclmanager.scaffolding.SclManagerConfig
 import com.lthummus.sclmanager.servlets.dto.{ErrorMessage, Match, Player}
+import com.lthummus.sclmanager.util.MatchForfeits
 import org.jooq.DSLContext
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.{NotFound, Ok}
+import org.scalatra.{BadRequest, Forbidden, InternalServerError, NotFound, Ok}
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.swagger.{Swagger, SwaggerSupport}
+import org.slf4j.LoggerFactory
+
+object PlayerServlet {
+  private val Logger = LoggerFactory.getLogger("PlayerServlet")
+}
 
 class PlayerServlet(implicit dslContext: DSLContext, val swagger: Swagger) extends SclManagerStack with JacksonJsonSupport with SwaggerSupport {
   protected implicit lazy val jsonFormats: Formats = DefaultFormats ++ JodaTimeSerializers.all
@@ -74,5 +81,63 @@ class PlayerServlet(implicit dslContext: DSLContext, val swagger: Swagger) exten
       case None => NotFound(ErrorMessage(s"No player with id $name found"))
       case Some(player) => Ok(player)
     }
+  }
+
+  post("/:name/revoke") {
+
+    val name = params("name")
+
+    PlayerServlet.Logger.info("Beginning forfeit process for {}", name)
+
+    request.header("Authentication") match {
+      case Some(x) if x == SclManagerConfig.forfeitPassword => //nop
+      case _                                                => PlayerServlet.Logger.warn("Invalid password. Stopping."); halt(Forbidden("No"))
+    }
+
+    val matches = BoutDao.getMatchesForPlayer(name)
+
+    if (matches.isEmpty) {
+      PlayerServlet.Logger.warn("{} not found", name)
+      halt(NotFound(s"player $name not found"))
+    }
+
+    if (matches.exists(_.getStatus == 1)) {
+      //there are some played matches, deal with them manually
+      PlayerServlet.Logger.warn("Found some matches that were played. Stopping")
+      halt(BadRequest(s"Player $name has some played matches. You must unset them first"))
+    }
+
+    val forfeitWorklist = matches.map{ curr =>
+      //figure out who the winner is....
+      val winner = if (curr.getPlayer1 == name) {
+        curr.getPlayer2
+      } else if (curr.getPlayer2 == name) {
+        curr.getPlayer1
+      } else {
+        PlayerServlet.Logger.warn("We found a match belonging to a player that they are not participating in. This should never happen")
+        halt(InternalServerError("We found a match belonging to a player that they are not participating in. This should never happen"))
+      }
+
+      (curr.getId, winner, curr.getStatus)
+    }
+
+    PlayerServlet.Logger.info("Built forfeit worklist of {} matches", forfeitWorklist.size)
+
+    val results = forfeitWorklist.map{ case (id, winner, status) =>
+      if (status == 2) {
+        //already forfeited, so don't touch it
+        s"Skipping match $id because already forfeited"
+      } else {
+        PlayerServlet.Logger.info("Forfeiting match {}", id)
+        val currResult = MatchForfeits.forfeitMatch(id, winner, s"$name Dropout")
+        if (currResult.isRight) {
+          s"Success forfeiting match $id"
+        } else {
+          s"Failed forfeiting match $id"
+        }
+      }
+    }
+
+    Ok("results" -> results)
   }
 }
---
+++ src/main/scala/com/lthummus/sclmanager/util/MatchForfeits.scala
@@ -0,0 +1,54 @@
+package com.lthummus.sclmanager.util
+
+import com.lthummus.sclmanager.database.dao.{BoutDao, PlayerDao}
+import zzz.generated.tables.records.BoutRecord
+import scalaz._
+import Scalaz._
+import com.lthummus.sclmanager.database.TransactionSupport
+import org.jooq.DSLContext
+
+object MatchForfeits extends TransactionSupport {
+  private def checkStatus(bout: BoutRecord) = {
+    if (bout.getStatus == 0) {
+      ().right
+    } else {
+      "Match has already been played or forfeited".left
+    }
+  }
+
+  def forfeitMatch(id: Int, winner: String, text: String)(implicit dslContext: DSLContext) = {
+    def winnerIsValid(winner: String, player1Name: String, player2Name: String) = {
+      if (winner == player1Name || winner == player2Name)
+        ().right
+      else
+        s"$winner is not valid for this match".left
+    }
+
+    insideTransaction { implicit dslContext =>
+      for {
+        bout <- BoutDao.getById(id) \/> "There is no match with that id"
+        _    <- winnerIsValid(winner, bout.getPlayer1, bout.getPlayer2)
+        _    <- checkStatus(bout)
+        _    <- BoutDao.updateBoutForfeitStatus(id, winner, text)
+        _    <- PlayerDao.postResult(bout.getPlayer1, if (bout.getPlayer1 == winner) "win" else "loss")
+        _    <- PlayerDao.postResult(bout.getPlayer2, if (bout.getPlayer2 == winner) "win" else "loss")
+      } yield {
+        bout
+      }
+    }
+  }
+
+  def doubleForfeit(id: Int, text: String)(implicit dslContext: DSLContext) = {
+    insideTransaction { implicit dslContext =>
+      for {
+        bout <- BoutDao.getById(id) \/> "No bout found with id"
+        _    <- checkStatus(bout)
+        _    <- BoutDao.updateBoutDoubleForfeit(id, text)
+        _    <- PlayerDao.postResult(bout.getPlayer1, "loss")
+        _    <- PlayerDao.postResult(bout.getPlayer2, "loss")
+      } yield {
+        bout
+      }
+    }
+  }
+}
